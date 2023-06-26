2cd3fa607e97848c1d671a74cd391bfce6a048f8
nitanshuj <nitanshuj138@gmail.com>
2023-06-26 16:52:38 -0400

    Transformation + Notebook

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,7 +6,7 @@ import numpy as np, pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
 
 from src.exception import CustomException
 from src.logger import logging
@@ -30,15 +30,15 @@ class DataTransformation:
             # Pipeline for Categorical Variables
             num_pipeline = Pipeline(
                 steps = [
-                    ("imputer", SimpleImputer(strategy="median")),
+                   # ("imputer", SimpleImputer(strategy="median")),
                     ("scalar", StandardScaler())                   
                 ]) 
 
             # Pipeline for Categorical Variables
             cat_pipeline = Pipeline(
                 steps=[
-                    ("imputer", SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder", OneHotEncoder())
+                    #("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OrdinalEncoder())
                     ("scalar", StandardScaler())
                 ]
             )
@@ -67,44 +67,54 @@ class DataTransformation:
             logging.info("Reading of Train and Test data completed")
             logging.info("Obtaining preprocessing object")
 
-            preprocessor_obj = self.get_data_transformer_object()
+            preprocessing_obj = self.get_data_transformer_object()
 
-            target_column = "is_fraud"
-            
-            # numerical_columns = ["writing_score", "reading_score"]
+            target_column_name = "is_fraud"            
+            numerical_columns = ["amt"]
+            categorical_columns = ["category", "gender", "job", "city", ""]
 
-            # input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            # target_feature_train_df=train_df[target_column_name]
+            """
+            =========================================================
+            We have 12 numerical features : 
+            ['trans_date_trans_time', 'cc_num', 'amt', 
+            'zip', 'lat', 'long', 'city_pop', 'dob', 'unix_time', 
+            'merch_lat', 'merch_long', 'is_fraud']
+            =========================================================
+            We have 10 categorical features : 
+            ['merchant', 'category', 'first', 'last', 
+            'gender', 'street', 'city', 'state', 'job', 
+            'trans_num']
+            =========================================================
+            """
 
-            # input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            # target_feature_test_df=test_df[target_column_name]
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
 
-            # logging.info(
-            #     f"Applying preprocessing object on training dataframe and testing dataframe."
-            # )
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
-            # input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            # input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe.")
 
-            # train_arr = np.c_[
-            #     input_feature_train_arr, np.array(target_feature_train_df)
-            # ]
-            # test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
-            # logging.info(f"Saved preprocessing object.")
+            train_arr = np.c_[input_feature_train_arr, 
+                              np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, 
+                             np.array(target_feature_test_df)]
 
-            # save_object(
+            logging.info(f"Saved preprocessing object.")
 
-            #     file_path=self.data_transformation_config.preprocessor_obj_file_path,
-            #     obj=preprocessing_obj
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
 
-            # )
-
-            # return (
-            #     train_arr,
-            #     test_arr,
-            #     self.data_transformation_config.preprocessor_obj_file_path,
-            # )
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
             
         except Exception as e:
             raise CustomException(e, sys)
